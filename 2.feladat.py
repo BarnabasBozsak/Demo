2fee3ef2ae4bf245fa6a4440396eb06a8e57317b
Deák Máté <b3deamat@vasvari.org>
2025-09-29 14:03:09 +0200

    2.feladat

---
+++ 2.feladat.py
@@ -0,0 +1,6 @@
+oldal1=int(input("Adja meg a téglalap egyik oldalát:"))
+oldal2=int(input("Adja meg a téglalap egyik oldalát:"))
+Kerulet=(oldal1+oldal2)*2
+Terulet=oldal1*oldal2
+print("Kerulet:",Kerulet)
+print("Terület:",Terulet)
--- Python/aremeles.py
+++
@@ -1,3 +0,0 @@
-ar=int(input("Kérem a termék árát!"))
-aremeles=float(input("Kérem az áremelést százalékban!"))
-print("A megemelt ár:",ar/100*(100+aremeles))
